018e75f0e3a2e87e9e38fb9b9d2059b5f5464229
DanQuixot <mrborisov1@gmail.com>
2019-01-29 16:44:52 +0000

    added collision detection logic and random appearing apples

--- canvas.js
+++ canvas.js
@@ -1,40 +1,45 @@
-let canvas = document.querySelector('canvas');
+let canvas = document.getElementById('canvas');
 
 // Set canvas dimensions
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight - 4;
 
 let c = canvas.getContext('2d');
 
 // Snake's original dimensions
-  let x = 100;  //Start Position X
-  let y = 100;  //Start Position Y
+  let x = canvas.width / 2;  //Start Position X
+  let y = canvas.height / 2;  //Start Position Y
   let width = 20; // Start width
   let height = 20; // Start height
   let speed = 5; // Start Speed
   let dx = 0; // Start Direction X
   let dy = 0; // Start Direction Y
 
+// Apple's original dimensions
+  let ax = 50; // Apples's Start Position X
+  let ay = 50; // Apples's Start Position Y
+  let appleRadius = 10; // Apple's Radius
+
 // Set background function
 let background = function() {
   c.fillStyle = "gray";
-  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.fillRect(0, 4, canvas.width, canvas.height);
   c.stroke();
 }
 
-// Update Snake's direction
+// Update Snake's direction - edge of a screen
 let update = function() {
-  if (x > innerWidth) {
+  if (x > canvas.width) {
     x = 0;
   } 
-  if (y > innerHeight) {
+  if (y > canvas.height) {
     y = 0;
   }
-  if (x < 0) {
-    x = innerWidth;
+  if ( x < 0 ) {
+    x = canvas.width;
   }
-  if (y < 0) {
-    y = innerHeight;
+  if ( y < 0 ) {
+    y = canvas.height;
   }
   x += dx;
   y += dy;  
@@ -48,7 +53,29 @@ let drawHead = function() {
   c.stroke();
 }
 
-// Arrows listener
+// Draw an Apple
+let drawApple = function() {
+  c.beginPath();
+  c.fillStyle = 'gold';
+  c.arc(ax, ay, appleRadius, 0, Math.PI * 2, true);
+  c.fill();
+  console.log(canvas.width, canvas.height)
+  console.log(ax, ay);
+}
+
+// Collision Detection System and new apple coordinates
+let collisionDetection = function() {
+  let res;
+  xDistance = x + 10 - ax;
+  yDistance = y + 10 - ay;
+  res = Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  if (res < 17) {
+    ax = Math.random() * (canvas.width - appleRadius * 2) + appleRadius;
+    ay = Math.random() * (canvas.height - appleRadius * 2) + appleRadius;
+  }
+}
+
+// Key directions listener
 document.addEventListener('keydown', function(e) {
   if (e.keyCode === 37 || e.keyCode === 65) {
     dy = 0;
@@ -66,16 +93,17 @@ document.addEventListener('keydown', function(e) {
     dx = 0;
     dy = speed;
   }  
-  console.log(e.keyCode);
+  
 });
 
 // Start animation
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight);
+  c.clearRect(0, 0, canvas.width, canvas.height);
   background();
+  drawApple();
   update();
   drawHead();
-  
+  collisionDetection();
 } 
 animate();
